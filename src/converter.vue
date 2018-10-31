daee52f3663665d4e1b09d7f9d31bca678fea250
Alessandro De Carli <a.decarli@papers.ch>
2018-10-31 14:15:00 +0100

    feat(text): adds text support

--- src/converter.vue
+++ src/converter.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Hypergate Certificate Converter</h1>
+    <h2>Certificate Input</h2>
     <el-upload
       action=""
       :on-change="convert"
@@ -10,14 +11,27 @@
     <el-button size="small" type="primary">Select *.pem Certificate</el-button>
       <div slot="tip" class="el-upload__tip">*.pem file of your certificate</div>
     </el-upload>
-
+    <p> - or - </p>
     <el-input
       ref="text" 
       type="textarea"
       :rows="10"
+      v-model="rawCertificate">
+    </el-input>
+    <br />
+    <br />
+    <el-button @click="convert()">Convert</el-button>
+
+    <h2>Converted Certificate</h2>
+    <el-input
+      ref="text" 
+      type="textarea"
+      readonly
+      :rows="10"
       v-model="convertedCertificate">
     </el-input>
-    
+    <br />
+    <br />
     <el-button @click="copy()" :disabled="convertedCertificate.length === 0">Copy</el-button>
   </div>
 </template>
@@ -47,31 +61,37 @@ export default {
         },
         convert (name, files) {
             // handle file changes
-            if (!files.length) return
-            if (!files || files.length === 0) return
+            if (!files || !files.length || files.length === 0) {
+                this.convertedCertificate = this.convertPem(this.rawCertificate)
+                Message({
+                    message: 'Certificate converted.',
+                    type: 'success'
+                })
 
-            const certFile = files[0].raw
-            const reader = new FileReader()
+            } else {
+                const certFile = files[0].raw
+                const reader = new FileReader()
 
-            reader.onload = (e) => {
-              let certificateContent = ''
-              let bytes = new Uint8Array(e.target.result)
-              let length = bytes.byteLength
-              
-              for (var i = 0; i < length; i++) {
-                certificateContent += String.fromCharCode(bytes[i])
-              }
+                reader.onload = (e) => {
+                  let certificateContent = ''
+                  let bytes = new Uint8Array(e.target.result)
+                  let length = bytes.byteLength
+                  
+                  for (var i = 0; i < length; i++) {
+                    certificateContent += String.fromCharCode(bytes[i])
+                  }
 
-              this.convertedCertificate = this.convertPem(certificateContent)
+                  this.convertedCertificate = this.convertPem(certificateContent)
 
-              Message({
-                message: 'Certificate converted.',
-                type: 'success'
-              })
+                  Message({
+                    message: 'Certificate converted.',
+                    type: 'success'
+                  })
 
+                }
+
+                reader.readAsArrayBuffer(certFile)
             }
-
-            reader.readAsArrayBuffer(certFile)
         }
     },
     data () {
